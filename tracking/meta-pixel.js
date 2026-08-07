250434746aa9b4db63abe3ff721fc4568c155d83
ritesh-performance <ritiesh.h@creativeamigos.in>
2026-08-07 12:50:15 +0530

    Add funnel event examples

--- tracking/meta-pixel.js
+++ tracking/meta-pixel.js
@@ -4,3 +4,14 @@ fbq('track', 'Purchase', {
   value: 1499.00,
   currency: 'INR'
 });
+// User added a product to cart
+fbq('track', 'AddToCart');
+
+// User started checkout
+fbq('track', 'InitiateCheckout');
+
+// User completed purchase
+fbq('track', 'Purchase', {
+  value: 1499.00,
+  currency: 'INR'
+});
